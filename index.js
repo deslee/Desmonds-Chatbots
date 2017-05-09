62592fbdf4d5f70622b245dc855f76a5f9bea0e9
Desmond Lee <Desmond.Lee@davita.com>
2017-05-09 18:31:51 -0500

    reddit test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 console.log('Loading function');
 
+var reddit = require('./util/reddit');
+
 /**
  * Demonstrates a simple HTTP endpoint using API Gateway. You have full
  * access to the request and response payload, including headers and
@@ -28,8 +30,10 @@ exports.handler = (event, context, callback) => {
             done(new Error(`Unsupported method "${event.httpMethod}"`));
             break;
         case 'GET':
+            if (event.queryStringParameters.subreddit) {
+              reddit(event.queryStringParameters.subreddit, done);
+            }
             //done(new Error(`Unsupported method "${event.httpMethod}"`));
-            done(null, "hello world");
             break;
         case 'POST':
             done(null, event.body);
